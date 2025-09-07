f3f2e4c69124cc8fb074f7ea1f62ee8251a4921c
James Trotter <james.trotter@joingotu.com>
2025-09-07 18:48:05 -0400

    fix: indexes

--- db/migrations/20250907183807_add_indexes.up.sql
+++ db/migrations/20250907183807_add_indexes.up.sql
@@ -8,7 +8,6 @@ CREATE INDEX idx_user_list_deleted_at ON user_list(deleted_at);
 -- Add indexes for user_anime table
 CREATE INDEX idx_user_anime_user_id ON user_anime(user_id);
 CREATE INDEX idx_user_anime_anime_id ON user_anime(anime_id);
-CREATE UNIQUE INDEX idx_user_anime_user_id_anime_id ON user_anime(user_id, anime_id);
 CREATE INDEX idx_user_anime_list_id ON user_anime(list_id);
 CREATE INDEX idx_user_anime_status ON user_anime(status);
 CREATE INDEX idx_user_anime_user_id_status ON user_anime(user_id, status);
